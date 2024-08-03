af9b071a1903805c8dfd4a576a2b21fb27e8331c
millerserhii <millerserhii@gmail.com>
2024-08-03 10:23:39 +0200

    added terraform action

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,12 +1,11 @@
 resource "aws_db_instance" "webapp_postgres_db" {
-    auto_minor_version_upgrade = true
-    storage_type = "standard"
-    engine = "postgres"
-    engine_version = "14"
-    instance_class = "db.t2.micro"
-    # TODO: Change to variables
-    username = "postgres"
-    password = "postgres"
-    skip_final_snapshot = true
-    allocated_storage = 20
+  auto_minor_version_upgrade = true
+  storage_type               = "standard"
+  engine                     = "postgres"
+  engine_version             = "14"
+  instance_class             = "db.t2.micro"
+  username                   = var.db_username
+  password                   = var.db_password
+  skip_final_snapshot        = true
+  allocated_storage          = 20
 }
